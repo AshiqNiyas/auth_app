382444f89c32fad0b2768767906cfb0b561307d8
Ashiq N <ashiqniyas.dev@gmail.com>
2025-07-09 21:21:10 +0530

    cors

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -78,7 +78,7 @@ router.post("/login", async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "Lax",
+      sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
       maxAge: 3600000, // 1 hour
     });
 
